6c5ba283fdeed19af892ff3298235fee516329d3
konrad <konradwodnicki98@gmail.com>
2021-01-16 20:42:34 +0100

    initial commit

---
+++ src/main/java/com/konrad/dietApp/DietAppApplication.java
@@ -0,0 +1,13 @@
+package com.konrad.dietApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DietAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DietAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/konrad/dietApp/DietAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.konrad.dietApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DietAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
