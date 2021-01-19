455b0c58cbc0960279f9f199cad5d0705a7735cf
konrad <konradwodnicki98@gmail.com>
2021-01-19 11:10:36 +0100

    add window for your own meal and logic for this

--- src/main/java/com/konrad/dietApp/meal/MealController.java
+++ src/main/java/com/konrad/dietApp/meal/MealController.java
@@ -4,8 +4,10 @@ import com.konrad.dietApp.product.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 
@@ -25,6 +27,20 @@ public class MealController {
     @RequestMapping("/meals/delete/{id}")
     public String deleteMeal(@PathVariable(name="id") int id){
         mealService.deleteMeal(id);
+        return "redirect:/meals";
+    }
+    @RequestMapping("/new_meal")
+    public String showNewMealForm(Model model){
+        Meal meal= new Meal();
+        model.addAttribute("meal", meal);
+
+        return "new_meal";
+    }
+
+    @RequestMapping(value="/save", method= RequestMethod.POST)
+    public String saveProduct(@ModelAttribute("meal") Meal meal){
+        mealService.save(meal);
+
         return "redirect:/meals";
     }
 
